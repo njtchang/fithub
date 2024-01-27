117deabcd32bf88ebb48c8ed21c6c833819cfb75
Lawrence Chau <chaulawrencejh@gmail.com>
2024-01-27 13:20:08 -0800

    created upload button

---
+++ frontendNew/src/routes/upload/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<div class="instruction">
+    Upload Photo Here
+</div>
+
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<form>
+    <div class = "buttonlabel">
+        <label for="file"></label>
+        <input
+            type="file"
+            id="file"
+            name="fileToUpload"
+            accept=".jpg, .jpeg, .png, .webp"
+            required
+        />
+    <button type="submit">Submit</button>
+</form>
+
+
+<style>
+    .buttonlabel {
+        text-align: center;
+    }
+
+    .instruction {
+        text-align: center;
+    }
+</style>
