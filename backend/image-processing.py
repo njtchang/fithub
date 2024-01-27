21ad52d76a22cf56017d24e04214b66b634afe99
Stanley Sha <stsha2023@gmail.com>
2024-01-27 11:04:55 -0800

    group image-processing into function so they can be called by other py files

--- backend/image-processing.py
+++ backend/image-processing.py
@@ -7,4 +7,10 @@ output_path = 'C:\\Users\\stsha\\fithub\\backend\\tshirtnobg.png'
 input = Image.open(input_path)
 output = remove(input)
 
-output.save(output_path)
+output_crop = output.crop(output.getbbox())
+output_crop.save(output_path)
+array = list(output_crop.getdata(3))
+
+output_crop.save(output_path)
+
+print(array)
