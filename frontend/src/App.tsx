45313750df329218f5a973aa5c73970e18ca85b3
sevenwastaken <angie6cheng@gmail.com>
2024-01-26 22:37:10 -0800

    added initial frontend react folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="jffmonster">app</div>
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "FF5B66",
+        "secondary-400": "#FFCD58",
+        "secondary-500": "FFC132",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/temphomepage.png')"
+      }),
+      fontFamily: {}
+    },
+  },
+  plugins: [],
+};
