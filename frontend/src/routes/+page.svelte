5dca5510c477edcbf76ad5558eef6c585a1429bc
sevenwastaken <angie6cheng@gmail.com>
2024-01-27 14:27:41 -0800

    added the logo to the nav bar and added the outfit display to the home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <nav>
+    <div class="nav-logo">
+        <img src="src/lib/pictures/logo.png" alt="Your Logo" />
+    </div>
     <ul>
         <li>
             <a href="/">Home</a>
@@ -9,7 +12,40 @@
     </ul>
 </nav>
 
+<body>
+    <div class="outfit-side">
+        <header>
+            <h1>Outfit</h1>
+        </header>
+        <img src="src/lib/pictures/temp-outfit.png" alt="outfit" />
+    </div>
+    <div class="gallery-side">
+        <header>
+            <h1>Gallery</h1>
+        </header>
+    </div>
+</body>
+
 <style>
+    div.nav-logo>img{
+        width: 200px;
+        length: 200px;
+        margin: 0;
+    }
+    div.outfit-side {
+        width: 50%;
+        background-color: bisque;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    div.outfit-side>img {
+        width: 50%;
+        height: 75%;
+    }
+    div.gallery-side {
+        width: 50%;
+    }
     nav {
         display: flex;
         background-color: lightblue;
