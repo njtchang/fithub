b48ae3140289b162887423e614960c845a477d60
sevenwastaken <angie6cheng@gmail.com>
2024-01-27 18:52:51 -0800

    finished drop down menu and made options into buttons for later use, the drop down is a little boof though

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,9 @@
 <script>
-    // let isDropdownOpen = false // default state (dropdown close)
 
-    // const handleDropdownClick = () => {
-    //     isDropdownOpen = !isDropdownOpen // togle state on click
-    // }
-
-    // const handleDropdownFocusLoss = ({ relatedTarget, currentTarget }) => {
-    //     // use "focusout" event to ensure that we can close the dropdown when clicking outside or when we leave the dropdown with the "Tab" button
-    //     if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return // check if the new focus target doesn't present in the dropdown tree (exclude ul\li padding area because relatedTarget, in this case, will be null) 
-    //     isDropdownOpen = false
-    // }
-
-  let isOpen = false;
-  let items = ["Item 1", "Item 2", "Item 3"];
+  let shirtIsOpen = false;
+  let pantIsOpen = false;
+  let shirtItems = ["Shirt 1", "Shirt 2", "Shirt 3"];
+  let pantItems = ["Pant 1", "Pant 2", "Pant 3", "pant 4"]
 
 </script>
 
@@ -36,69 +27,35 @@
             <header>
                 <h1>Outfit</h1>
             </header>
-            <img src="src/lib/pictures/temp-outfit.png" alt="outfit" />
+            <img src="src/lib/pictures/combo_outfit.png" alt="outfit" />
         </div>
         <div class="gallery-side">
             <header>
                 <h1>Gallery</h1>
             </header>
-            <!-- <div class="flex justify-between items-center">
-                <div class="dropdown" on:focusout={handleDropdownFocusLoss}>
-                    <button class="btn m-1" on:click={handleDropdownClick} >
-                    {#if isDropdownOpen}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            class="inline-block h-6 w-6 stroke-current">
-                            <title>Close Dropdown</title>
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                        {:else}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            class="inline-block h-6 w-6 stroke-current">
-                            <title>Open Dropdown</title>
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                        {/if}
-                    </button>
-                    <ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52" style:visibility={isDropdownOpen ? 'visible' : 'hidden'}>
-                        <li><button class="btn text-slate-300">Item 1</button></li>
-                        <li><button class="btn text-slate-300">Item 2</button></li>
-                    </ul>
-                </div>
-                <p class="text-slate-300">
-                    isDropdownOpen: {isDropdownOpen}
-                </p>
-            </div> -->
-                <div class="menu">
-            <button on:click={() => (isOpen = !isOpen)}>Dropdown</button>
+            <div class="menu">
+                <button on:click={() => (shirtIsOpen = !shirtIsOpen)}>Shirts</button>
 
-            {#if isOpen}
-                <div
-                class="absolute left-0 w-48 py-2 mt-2 mr-10 bg-white rounded-lg shadow-xl"
-                >
-                {#each items as item (item)}
-                    <a
-                    href="#"
-                    class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-                    >
-                    {item}
-                    </a>
+                {#if shirtIsOpen}
+                <div class="dropbuttons">
+                {#each shirtItems as sitem (sitem)}
+                    <button>{sitem}</button>
                 {/each}
                 </div>
-            {/if}
+                {/if}
+
+                <button on:click={() => (pantIsOpen = !pantIsOpen)}>Pants</button>
+
+                {#if pantIsOpen}
+                <div class="dropbuttons">
+                {#each pantItems as pitem (pitem)}
+                    <!-- <a href="#top">
+                    {pitem}
+                    </a> -->
+                    <button>{pitem}</button>
+                {/each}
+                </div>
+                {/if}
             </div>
         </div>
     </div>
@@ -137,6 +94,41 @@
         margin-top: 1rem; 
         color: #ffffff; 
         background-color: #3B82F6; 
+        width: 12rem;
+    }
+    div.dropbuttons {
+        left: 0; 
+        /* padding-top: 0.5rem;
+        padding-bottom: 0.5rem;  */
+        margin-top: 0.5rem; 
+        margin-right: 2.5rem; 
+        border-radius: 0.5rem; 
+        width: 12rem; 
+        background-color: #ffffff; 
+        box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04); 
+    }
+    div.dropbuttons>a {
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem; 
+        padding-left: 1rem;
+        padding-right: 1rem; 
+        color: #2b3441;
+        display: flex;
+        flex-direction: column;
+    }
+    div.dropbuttons>a:hover {
+        background-color: #d3e3e9; 
+    }
+    div.dropbuttons>button {
+        display: flex;
+        flex-direction: column;
+        padding: 15px;
+        border: none;
+        background-color: aliceblue;
+        width: 100%;
+    }
+    div.dropbuttons>button:hover {
+        background-color: #d3e3e9;
     }
     nav {
         display: flex;
